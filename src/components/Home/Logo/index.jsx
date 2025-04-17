f7bf16d63e64fed92a6e4d68748a8ffcceb4edd1
ShantanuTK <shantanu.kx@gmail.com>
2025-04-17 10:16:21 -0400

    gsap bug fix: attempt-2

--- src/components/Home/Logo/index.jsx
+++ src/components/Home/Logo/index.jsx
@@ -10,7 +10,7 @@ const Logo = () => {
 	const solidLogoRef = useRef();
 
 	useEffect(() => {
-		gsap.registerPlugin(DrawSVGPlugin);
+		// gsap.registerPlugin(DrawSVGPlugin);
 
 		gsap.timeline().to(bgRef.current, {
 			duration: 0,
